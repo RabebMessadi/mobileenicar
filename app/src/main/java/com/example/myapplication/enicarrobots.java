3d858c6a7762f19a344ecc89c50c5660ba9f6524
RabebMessadi <bboubbamessadi@gmail.com>
2020-12-06 22:52:18 +0100

    2

--- app/src/main/java/com/example/myapplication/enicarrobots.java
+++ app/src/main/java/com/example/myapplication/enicarrobots.java
@@ -2,13 +2,38 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class enicarrobots extends AppCompatActivity {
-
-    @Override
+ public Button butcomp;
+ public Button butback;
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enicarrobots);
+        butcomp = (Button) findViewById(R.id.butcompitition);
+        butcomp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(enicarrobots.this , compitition.class);
+                startActivity(intent);
+            }
+        });
+        butback = (Button) findViewById(R.id.back);
+        butback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentback =new Intent(enicarrobots.this , evenements.class);
+                startActivity(intentback);
+            }
+        });
+
+        /*** Rest Planning  view ***/
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/evenements.java
+++ app/src/main/java/com/example/myapplication/evenements.java
@@ -1,13 +1,34 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class evenements extends AppCompatActivity {
+    public Button enicarrobots;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.evenements);}
+        setContentView(R.layout.evenements);
+
+        /**** enicarrobots 5.0 to the next view od enicarrobots****/
+        enicarrobots = (Button) findViewById(R.id.robotsevent);
+        enicarrobots.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(evenements.this,enicarrobots.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+
+
+
 }
